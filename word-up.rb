0416dcc7c39387711f47774ce9c3dadeedddb0b6
Andrew Silbersmith <asilbersmith11@gmail.com>
2013-08-12 03:33:03 -0400

    started laying all of the inputs that will be needed to build a new word cloud.

--- word-up.rb
+++ word-up.rb
@@ -7,6 +7,9 @@ testing key = kK6yOtFp1BD7izLGsUfy4HwPjWXHR2lj
 mashape = "https://gatheringpoint-word-cloud-maker.p.mashape.com/index.php"
 
 #
+def post_api
+return Unirest::post
+end
 
 def post_address(mashape)
 puts "#{mashape}"
@@ -16,10 +19,20 @@ def post_method
 	return Unirest::post
 end
 
-def authorization_hash#will add method for getting theirs later
+def user_authorization#will add method for getting theirs later
 	authorization = {"X-Mashape-Authorization" => "kK6yOtFp1BD7izLGsUfy4HwPjWXHR2lj"}
 	return authorization
+end 
 
+
+
+def user_inputs(height, textblock, width, config)
+	return height, textblock, width, config
+end
+
+
+def get_word_cloud(post_api, post_address, user_authorization, user_inputs)
+	"#{post_api}", "#{post_address}", "#{user_authorization}", "#{user_inputs}"
 end 
 
 
@@ -30,9 +43,47 @@ end
 
 
 
+
+
+user_authorization = Hash.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#m = Unirest::post("https://gatheringpoint-word-cloud-maker.p.mashape.com/index
+def inputs(height, textblock, width,config )
+
+
+
 binding.pry
 
 
 def
 
 	key token 
+
+	def initialize ()
+
+
+
+
+class Person
+
+class WordCloudMaker
+
+class WordCloud
+
+
