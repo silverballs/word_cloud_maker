ee04b937317258f8e3d3bbb977b1f2e390b8260e
Andrew Silbersmith <asilbersmith11@gmail.com>
2013-08-12 03:06:15 -0400

    started creating methods to keep it dry.  going to set most of the methods for now to make sure it all works before i move on to building classes and what not

--- word-up.rb
+++ word-up.rb
@@ -2,4 +2,37 @@ require 'pry'
 require 'unirest'
 require 'JSON'
 
+testing key = kK6yOtFp1BD7izLGsUfy4HwPjWXHR2lj
 
+mashape = "https://gatheringpoint-word-cloud-maker.p.mashape.com/index.php"
+
+#
+
+def post_address(mashape)
+puts "#{mashape}"
+end
+
+def post_method
+	return Unirest::post
+end
+
+def authorization_hash#will add method for getting theirs later
+	authorization = {"X-Mashape-Authorization" => "kK6yOtFp1BD7izLGsUfy4HwPjWXHR2lj"}
+	return authorization
+
+end 
+
+
+
+
+
+
+
+
+
+binding.pry
+
+
+def
+
+	key token 
