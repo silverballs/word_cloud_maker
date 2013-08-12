655f4a604873daf176067320b7addddfdbde6f1b
Andrew Silbersmith <asilbersmith11@gmail.com>
2013-08-12 11:02:59 -0400

    got some methods more organized and testing output before building out some class methods

--- word-up.rb
+++ word-up.rb
@@ -1,51 +1,49 @@
 require 'pry'
-require 'unirest'
+require 'UniRest'
 require 'JSON'
 
 mashape = "https://gatheringpoint-word-cloud-maker.p.mashape.com/index.php"
 
-def post_api
-return Unirest::post
+def post_api(:gem, :post)
+return "Unirest::post"
 end
 
 def post_address(mashape)
 return"#{mashape}"
 end
 
-def post_method
-	return Unirest::post
-end
+#f Unirest::post
 #maybe do a key.each do value = the chomp of of question
 def user_input(method, url, headers, body)
 	user_input = {"method" => "","url" => "", "headers" => "", "body" => "" }
 end 
 
 def authorization(name,token)
-authorization = {}
+authorization = {"name" => "token"}
 end 
 
 
 class User
+User = Hash.new	
 	attr_reader(:token,:config)
 
 def initialize(token,info)
 	@token = authorization
 	@config= user_input
   end
+user
 
 end 
 
 
 
 
-post_api(post_address),User.token,User.config
+post_api("#{post_address(mashape)},#{User.token},#{User.config}")
 
 
 
 
 
-
-class WordCloud
 
 
 
