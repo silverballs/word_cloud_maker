c4743a34293bfb5dc969be960a6e670f8cea5347
Andrew Silbersmith <asilbersmith11@gmail.com>
2013-08-12 14:35:07 -0400

    added user class in the testing doc that seems to be working.  going to start organizing the main now

--- testing.rb
+++ testing.rb
@@ -10,23 +10,62 @@ require 'pry'
 # #=> {"name"=>"token"}
 
 
-
-words =[]
-# puts "Would you like to add word?"
-# answer = gets.chomp
-loop do 
-	puts "would you like to add a word?"
-	answer = gets.chomp
-	if answer.downcase != "yes"
-	break
-else 
-puts "Ok, give it to me"
-word = gets.chomp.to_s
-words << word
+ class User 
+ 	attr_reader :authorization
+	def initialize(authorization)
+	@authorization = {"X-Mashape-Authorization" => "kK6yOtFp1BD7izLGsUfy4HwPjWXHR2lj"}
+		end
 end 
-end 
+	
+andrew = User.new('authorization')
 
-words.class?
+print andrew 
+
+# authorization_hash = {"X-Mashape-Authorization" => "kK6yOtFp1BD7izLGsUfy4HwPjWXHR2lj"}
+
+# user_input(user,token)
+
+
+# authorization_hash
+
+
+# words =[]
+#  puts "Would you like to add word?"
+#  answer = gets.chomp
+# loop do 
+# 	puts "would you like to add a word?"
+# 	answer = gets.chomp
+# 	if answer.downcase != "yes"
+# 	break
+# else 
+# puts "Ok, give it to me"
+# word = gets.chomp.to_s
+# words << word
+# end 
+# end 
+
+
+
+
+
+# def user_authorize_input
+# puts "what is your user?"
+# user = gets.chomp.to_s
+# puts "what is your token?"
+# token = gets.chomp.to_s
+# authorize_input = {"user" => "token"}
+# puts "Ok thanks #{user}!.upcase"
+# return authorize_input 
+# end
+
+# user_authorize_input
+
+
+
+
+
+
+
 
 # puts words
 # puts words
