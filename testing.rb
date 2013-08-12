58c8342760900398176ea686b6fdb1c7e9ddcc68
Andrew Silbersmith <asilbersmith11@gmail.com>
2013-08-12 13:14:00 -0400

    added a testing.rb page for testing methods

---
+++ testing.rb
@@ -0,0 +1,47 @@
+require 'pry'
+require 'unirest'
+require 'JSON'
+
+
+def authorization(name,token)
+authorization = {"name" => "token"}
+end 
+puts authorization
+
+
+
+
+
+
+
+
+
+
+
+
+#going to use this for the request_post for now for testing.
+# api = Unirest::HttpClient
+# url= "https://gatheringpoint-word-cloud-maker.p.mashape.com/index.php"
+
+
+# #tested api of request_post and is  working fine.  
+# def request_post(api,url,authorization,
+# 	return api,url,@authorization
+# end 
+
+# pry.binding
+
+
+
+
+# api = Unirest::HttpClient
+# def x(api)
+# 	return api
+# end
+
+
+
+#what we need:
+#method for getting words then appending them to the hash of words
+#
+#
